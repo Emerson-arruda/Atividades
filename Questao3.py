f0083deefcb17a2da845b64d7b296d561576d1d3
Emerson-arruda <113213708+Emerson-arruda@users.noreply.github.com>
2022-09-26 12:47:52 -0300

    Add files via upload

---
+++ Questao3.py
@@ -0,0 +1,13 @@
+#início
+
+numb1: float; numb2: float
+
+numb1 = int(input("Digite um numero: "))
+numb2 = int(input("Digite outro numero: "))
+
+#processo
+
+if numb1>numb2:
+    print(f"O numero maior e {numb1}")
+else:
+    print(f"O numero maior e {numb2}")
